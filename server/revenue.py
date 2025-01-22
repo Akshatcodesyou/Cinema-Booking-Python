407e285dd44efefb1efa5ba53e368169d92da3aa
Akshat Singh <144692981+Akshatcodesyou@users.noreply.github.com>
2025-01-22 11:17:42 +0400

    Delete server directory

--- server/revenue.py
+++
@@ -1,9 +0,0 @@
-BOOK = "server2/bookings.txt"
-
-def view_revenue():
-    try:
-        with open(BOOK, "r") as file:
-            total = sum(float(line.strip().split(',')[3]) for line in file)
-        print(f"\nTotal Revenue: {total} AED\n")
-    except IOError:
-        print("Error reading revenue file.")
